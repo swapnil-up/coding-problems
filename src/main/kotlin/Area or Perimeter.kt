f4a294c716f4b7da3ca6589cb98fb8dc3547917e
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-08-17 10:24:01 +0545

    8 Kyu Area or Perimeter solved. This was very easy to be honest. Nothing specific to keep in mind. good to know that I can directly pass the test case in a println without declaring. Just call object.function
    I wasn't very far off from top most answer either
    
    object Solution {
      fun areaOrPerimeter(l: Int, w: Int) = if (l == w) l * w else 2 * (l + w)
    }

---
+++ src/main/kotlin/Area or Perimeter.kt
@@ -0,0 +1,10 @@
+object Solution {
+    fun areaOrPerimeter(l:Int, w:Int):Int {
+        if (l==w) return(l*l) else return(2*(l+w))
+    }
+}
+
+fun main() {
+    println(Solution.areaOrPerimeter(2,2))
+
+    }
