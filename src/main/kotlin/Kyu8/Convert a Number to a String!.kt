17fb32b6f0a8e0b9c40115b5b4671118d18f48f3
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-09-02 22:57:51 +0545

    8 Kyu Convert a Number to a String! solved. Way too easy. This was a cool solution too:
    fun numberToString(num: Int) = "$num"

---
+++ src/main/kotlin/Kyu8/Convert a Number to a String!.kt
@@ -0,0 +1,11 @@
+package Kyu8
+
+fun numberToString(num: Int): String {
+    return(num.toString())
+}
+
+fun main() {
+    println(numberToString(67))
+    println(numberToString(123))
+    println(numberToString(999))
+}
